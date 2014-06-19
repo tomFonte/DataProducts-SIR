2010c750bd63bc4f9635e4b2afca55a9b5189c16
tomFonte <tomas.fontecilla@omegastat.com>
2014-06-19 10:40:24 -0400

    New code

--- ui.R
+++ ui.R
@@ -1,31 +1,33 @@
 shinyUI(
   pageWithSidebar(
-    headerPanel("Shiny Application"),
+    headerPanel("SIR Model - Mathematical Epidemiology"),
     sidebarPanel(
-      numericInput("days","Days of disease",50),
-      radioButtons("radiobutton","Model type:",c("SIR"="sir","Exponential Growth"="growth")),
-      conditionalPanel(condition="input.radiobutton=='sir'",
-        sliderInput("beta",label="Transmission rate",min=0,max=5,value=2.5,step=0.1),
-        sliderInput("gama",label="days until recovery",min=0,max=15,value=2.5,step=0.1),
-        numericInput("Suceptible","Initial Suceptible Population",50),
-        numericInput("Infectious","Initial Infectious Population",1),
-        numericInput("Recovered","Initial Recovered Population",0)#, 
-        #actionButton("radiobutton","Submit")
+#       radioButtons("Instructions","Instructions",c("do not show","show")),
+#       conditionalPanel(condition="input.Instructions=='show'",
+#                        p("This application shows the most simple mathematical model applied to epidemiology. 
+#                        The next steps are necessary:
+#                        1.- Input the number of days for which the disease will be followed
+#                         slide the transmission rate, i.e., probability at which a suceptible person could become infected3.- slide the number of days that said person will be infecting others until recovered from  the disease
+#                        4.- Initialize values, indicating the suceptible population, already infected population, and already recovered population
+#                       ")),
+                       numericInput("days","Days of disease",50),
+                       sliderInput("beta",label="Transmission rate",min=0,max=1,value=0.5,step=0.01),
+                       sliderInput("gama",label="days until recovery",min=0,max=15,value=2.5,step=0.1),
+                       numericInput("Suceptible","Initial Suceptible Population",50),
+                       numericInput("Infectious","Initial Infectious Population",1),
+                       numericInput("Recovered","Initial Recovered Population",0)
+                       
       ),
-      conditionalPanel(condition="input.radiobutton=='growth'",
-                       sliderInput("beta",label="Growth rate",min=0,max=5,value=2.5,step=0.1),
-                       numericInput("Suceptible","Initial Suceptible Population",50)#,
-#                        actionButton("radiobutton","Submit")
-      )
-    ),
-  mainPanel(
-    textOutput("tiempo"),
-    textOutput("iniciales"),
-    textOutput("parametros"),
-    textOutput("R_0"),
-    plotOutput("Plot")   
-    
-    
-  )
+    mainPanel(
+      #These are tests runs.
+      h4("Resultados"),
+      verbatimTextOutput("Result"),
+      plotOutput("Plot"),
+      h4("Basic Reproductive Number"),
+      h4(textOutput("R_0"))
+            
+    )
   )
 )
+
+
