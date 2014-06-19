af0cf001787478dfd6bfd5f3090b4399f0e91e12
Tomas Fontecilla <tomas.fontecilla@omegastat.com>
2014-06-19 11:02:02 -0400

    ui.R of shiny application
    Here you can find the ui of the application

--- ui.R
+++ ui.R
@@ -2,14 +2,6 @@ shinyUI(
   pageWithSidebar(
     headerPanel("SIR Model - Mathematical Epidemiology"),
     sidebarPanel(
-#       radioButtons("Instructions","Instructions",c("do not show","show")),
-#       conditionalPanel(condition="input.Instructions=='show'",
-#                        p("This application shows the most simple mathematical model applied to epidemiology. 
-#                        The next steps are necessary:
-#                        1.- Input the number of days for which the disease will be followed
-#                         slide the transmission rate, i.e., probability at which a suceptible person could become infected3.- slide the number of days that said person will be infecting others until recovered from  the disease
-#                        4.- Initialize values, indicating the suceptible population, already infected population, and already recovered population
-#                       ")),
                        numericInput("days","Days of disease",50),
                        sliderInput("beta",label="Transmission rate",min=0,max=1,value=0.5,step=0.01),
                        sliderInput("gama",label="days until recovery",min=0,max=15,value=2.5,step=0.1),
@@ -19,7 +11,6 @@ shinyUI(
                        
       ),
     mainPanel(
-      #These are tests runs.
       h4("Resultados"),
       verbatimTextOutput("Result"),
       plotOutput("Plot"),
@@ -29,5 +20,4 @@ shinyUI(
     )
   )
 )
-
 
